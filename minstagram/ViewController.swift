dd9b2856ea1b72a2d6399cebdad5545a4bd3805d
sariogluemree <sariogluemree@gmail.com>
2025-02-19 12:55:33 +0300

    Initial Commit

---
+++ minstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minstagram
+//
+//  Created by Hüseyin Emre Sarıoğlu on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ minstagramTests/minstagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  minstagramTests.swift
+//  minstagramTests
+//
+//  Created by Hüseyin Emre Sarıoğlu on 19.02.2025.
+//
+
+import Testing
+@testable import minstagram
+
+struct minstagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
